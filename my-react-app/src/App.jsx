549f91483d285b26da5eab636cbbe32937afce9b
tildanyrvik <tilda.nyrvik@gmail.com>
2026-04-26 21:11:05 +0200

    browserrouter and routes created

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,17 @@
-import Project1 from "./components/project1";
-import Project2 from "./components/project2";
-import Project3 from "./components/project3";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import About from "./pages/home.jsx";
+import Home from "./pages/about.jsx";
 
 function App() {
 
   return (
     <>
-      <Project1 title = "The Dog Blog" githubLink = "https://github.com/tildanyrvik/the-dog-blog.git"></Project1>
-      <br />
-      <Project2 title = "Flexbox Project" githubLink = "https://github.com/tildanyrvik/flexbox-practice.git"></Project2>
-      <br />
-      <Project3 title = "Wedding Invite" githubLink = "https://github.com/tildanyrvik/wedding-invite-animation.git"></Project3>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- my-react-app/src/pages/about.jsx
+++ my-react-app/src/pages/about.jsx
@@ -2,7 +2,7 @@ import Project1 from "../components/project1.jsx";
 import Project2 from "../components/project2.jsx";
 import Project3 from "../components/project3.jsx";
 
-funcction About (){
+function About (){
     return (
         <div>
             <h2>About</h2>
