1269b535a8b68073ab6c2cbb8d0b12c06f144f59
tildanyrvik <tilda.nyrvik@gmail.com>
2026-04-26 21:31:01 +0200

    nav links created

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/home.jsx";
 import About from "./pages/about.jsx";
+import Navbar from "./navbar.jsx";
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ my-react-app/src/navbar.jsx
@@ -0,0 +1,16 @@
+import {Link} from "react-router-dom";
+
+function Navbar () {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
