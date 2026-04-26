37a060d8920ae7ee90fe51c7fb0bb96caecdfbb2
tildanyrvik <tilda.nyrvik@gmail.com>
2026-04-26 17:01:47 +0200

    fonts added

--- my-react-app/src/components/project1.jsx
+++ my-react-app/src/components/project1.jsx
@@ -7,17 +7,17 @@ function Project1 (){
     <div className="project1_container">
         <img className="project1_img" src={project1} alt="Website called Dog Blog with yellow background"/>
         <br />
-        <h2>The Dog Blog</h2>
-        <br />
-        <p> This project is from early on of doing a front end development course at Sundsgården Folkhögskola. 
+        <h2 className="heading">The Dog Blog</h2>
+        <p className="paragraph"> This project is from early on of doing a front end development course at Sundsgården Folkhögskola. 
             Using HTML and CSS, I created a simple website about different dog breeds and a link for an adoption 
             website. In this project, I learned to use basic CSS, such as adding background colors, image borders, 
             font colors and styling links. One challenge I faced was getting the link for "Read more about French 
             Bulldogs" to work, as the intention was for the link to take the user to the bottom of the page. 
             However, by looking carefully at my notes and watching the lesson again, I managed to solve this problem. 
             
-        <br>
-        </br>Github repository: https://github.com/tildanyrvik/the-dog-blog.git
+        <br />
+        
+        <p className="paragraph"><span className="github_repo">Github repository:</span> https://github.com/tildanyrvik/the-dog-blog.git</p>
         </p>
     </div>
     )
