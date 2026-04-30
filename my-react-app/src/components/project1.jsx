54f23041cc46f77e662465b21826339d3cc47d8c
tildanyrvik <tilda.nyrvik@gmail.com>
2026-04-30 14:05:25 +0200

    JS added to project1.jsx

--- my-react-app/src/components/project1.jsx
+++ my-react-app/src/components/project1.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
+import P1Popup from "./p1_popup.jsx";
 import "./project1.css";
 import project1 from "../assets/project1.png";
 
 function Project1 (props){
-
+cont [isOpen, setIsOpen] = useState(false);
     return (
     <div className="project1_container">
         <img className="project1_img" src={project1} alt="Website called Dog Blog with yellow background"/>
         <br />
         <h2 className="heading">{props.title}</h2>
         <br />
-        <button onClick={}>More information</button>
-        
+        <button onClick={() => setIsOpen(true)}>More information</button>
+        {isOpen && (
+            <P1Popup title = {popupInfo.title} info = {popupInfo.info} />
+        )}
   
     </div>
     )
