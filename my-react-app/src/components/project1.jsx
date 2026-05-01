d326677bf20cb8f14e7058442abc4bcc8f3cd36f
tildanyrvik <tilda.nyrvik@gmail.com>
2026-05-01 15:23:04 +0200

    project info added

--- my-react-app/src/components/project1.jsx
+++ my-react-app/src/components/project1.jsx
@@ -5,7 +5,12 @@ import project1 from "../assets/project1.png";
 
 function Project1 (props){
     const [popupInfo, setPopupInfo] = useState({
-        info: "information about project",
+        info: `This project is from early on of doing a front end development course at Sundsgården Folkhögskola. 
+            Using HTML and CSS, I created a simple website about different dog breeds and a link for an adoption 
+            website. In this project, I learned to use basic CSS, such as adding background colors, image borders, 
+            font colors and styling links. One challenge I faced was getting the link for 'Read more about French 
+            Bulldogs' to work, as the intention was for the link to take the user to the bottom of the page. 
+            However, by looking carefully at my notes and watching the lesson again, I managed to solve this problem.`,
         link: "Link to github",
     });
     const [isOpen, setIsOpen] = useState(false);
