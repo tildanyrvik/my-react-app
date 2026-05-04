747aee1aa84e52fc24d61ad1a39b12cab0a6242e
tildanyrvik <tilda.nyrvik@gmail.com>
2026-05-04 22:13:18 +0200

    filer, map and sum created and corrected

--- my-react-app/src/pages/portfolio.jsx
+++ my-react-app/src/pages/portfolio.jsx
@@ -4,36 +4,58 @@ import Project3 from "../components/project3.jsx";
 import {useState} from "react";
 
 function Portfolio (){
-	const [viewProjects, setViewProjects] = useState(false);
-	const lookAtProjects = () => {
-	if (viewProjects) {
-		setViewProjects(false);
-	} else {
-		setViewProjects(true);
-	}
-}
+        const [viewProjects, setViewProjects] = useState(false);
+        const lookAtProjects = () => {
+        if (viewProjects) {
+            setViewProjects(false);
+        } else {
+            setViewProjects(true);
+        }
+    }
 
- return (
-        <div>
-            <h2>Portfolio</h2>
+    const projects = [
+        { title: "The Dog Blog", tags: ["HTML", "CSS"],  component: <Project1 title="The Dog Blog" /> },
+        { title: "Flexbox Project", tags: ["HTML", "CSS", "Flexbox"], component: <Project2 title="Flexbox Project" githubLink="https://github.com/tildanyrvik/flexbox-practice.git" /> },
+        { title: "Wedding Invite", tags: ["HTML", "CSS", "Animation"], component: <Project3 title="Wedding Invite" githubLink="https://github.com/tildanyrvik/wedding-invite-animation.git" /> }
+    ];
 
-                <button onClick={lookAtProjects}>Look at projects</button>
+    const [search, setSearch] = useState("");
 
+    return (
+            <div>
+                <h2>Portfolio</h2>
 
+                    <button onClick={lookAtProjects}>Look at projects</button>
+
+                    <div>
+                        <input
+                        type="text"
+                        placeholder="Search project..."
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        />
+                    </div>
+            
                 {viewProjects && (
-                <div>
-                <Project1 title = "The Dog Blog"></Project1>
-                <br />
-                <Project2 title = "Flexbox Project" githubLink = "https://github.com/tildanyrvik/flexbox-practice.git"></Project2>
-                <br />
-                <Project3 title = "Wedding Invite" githubLink = "https://github.com/tildanyrvik/wedding-invite-animation.git"></Project3>
-                </div>
+                    <div>
+                        {projects
+                            .filter((project) =>
+                                project.title.toLowerCase().includes(search.toLowerCase()) ||
+                                project.tags.some((tag) => tag.toLowerCase().includes(search.toLowerCase()))
+                            )
+                                    .map((project, index) => (
+                                    <div key={index}>
+                                        {project.component}
+                                    </div>
+                            ))
+                        }
+                    </div>
+                  
                 )}
-        </div>
-    )
+            </div>
+        )
 }
-
-
+               
 export default Portfolio;
 
-
+    
